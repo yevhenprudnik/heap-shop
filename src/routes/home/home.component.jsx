21d35227e49743da94bc131a1532da08e06ff335
Yevhen Prudnik <yevhenprudnik@gmail.com>
2023-08-28 22:23:49 +0300

    Fix testing variable.

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -3,7 +3,6 @@ import Categories from '../../components/categories/categories.component';
 const Home = () => {
   return (
     <div>
-      <h1> 1 + {process.env.REACT_APP_TEST_ENV}</h1>
       <Categories />
     </div>
   );
